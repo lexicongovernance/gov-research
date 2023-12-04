28dd8b56a1c2afba9f0b7ca38dc32e9a4f87f3b1
Martin Benedikt Busch <bene.busch1992@gmail.com>
2023-12-04 16:19:10 +0100

    implement vanilla qv

--- arb_mvp/qv.models.ts
+++ arb_mvp/qv.models.ts
@@ -1,2 +1,24 @@
-let message: string = 'Hello World';
-console.log(message);
+import * as math from 'mathjs';
+
+function quadraticVoting(votes: number[]): [Record<number, number>, number] {
+  const quadraticVotesDict: Record<number, number> = {};
+
+  for (let agentI = 0; agentI < votes.length; agentI++) {
+    const quadraticVotesAgentI = math.sqrt(votes[agentI]) as number;
+    quadraticVotesDict[agentI] = quadraticVotesAgentI;
+  }
+
+  const sumQuadraticVotes = Object.values(quadraticVotesDict).reduce(
+    (acc, value) => acc + value,
+    0
+  );
+
+  return [quadraticVotesDict, sumQuadraticVotes];
+}
+
+// Example usage:
+const votes: number[] = [4, 9, 16];
+const [result, sum] = quadraticVoting(votes);
+
+console.log('Quadratic Votes:', result);
+console.log('Sum of Quadratic Votes:', sum);
